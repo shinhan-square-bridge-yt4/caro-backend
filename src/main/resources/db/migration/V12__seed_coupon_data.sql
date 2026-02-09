42a80ce93f5d865654a79f2dd389fc2aaafee249
jisukimtech <jisukim.tech@gmail.com>
2026-02-09 12:28:12 +0900

    feat: 리워드 쿠폰 시드 데이터 추가

---
+++ src/main/resources/db/migration/V12__seed_coupon_data.sql
@@ -0,0 +1,3 @@
+-- 맥도날드 (reward_brand_id = 5)
+INSERT INTO reward_coupon (reward_brand_id, item_name, required_points, image_url, redeem_count, created_at) VALUES
+(5, '치즈스틱 교환권', 1, '/images/rewards/dummy.jpg', 6, NOW());
